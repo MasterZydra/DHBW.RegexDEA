55fd23e33fa48560c22eed6f012599b4054ada18
David Hein <david-hein196@web.de>
2020-01-23 10:52:46 +0100

    Add syntax node implementations

---
+++ src/OperandNode.java
@@ -0,0 +1,15 @@
+public class OperandNode extends SyntaxNode implements Visitable {
+    public int position;
+    public String symbol;
+
+    public OperandNode(String symbol) {
+        this.position = -1; // = not initialized
+        this.symbol = symbol;
+    }
+
+    @Override
+    public void accept(Visitor visitor)
+    {
+        visitor.visit(this);
+    }
+}
