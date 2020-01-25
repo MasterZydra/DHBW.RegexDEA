d244db687980330034394a8e5089d8fcd04f0756
David Hein <david-hein196@web.de>
2020-01-25 18:20:53 +0100

    Test logic for combined test

--- src/RegexDEA.java
+++ src/RegexDEA.java
@@ -1,6 +1,26 @@
-import test.ParserTest;
+import main.*;
+
+import java.util.Scanner;
 
 public class RegexDEA {
     public static void main(String[] args){
+        System.out.println(" ___   ____  __    ____  _         ___   ____   __");
+        System.out.println("| |_) | |_  / /`_ | |_  \\ \\_/     | | \\ | |_   / /\\");
+        System.out.println("|_| \\ |_|__ \\_\\_/ |_|__ /_/ \\     |_|_/ |_|__ /_/--\\");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n\nRegular expression (needs to be in parenthesis and end with '#'):");
+        String input = scanner.next();
+
+        Visitable syntaxTree = null;
+
+        // 1. Parse to syntax tree
+        Parser parser = new Parser(input);
+        try {
+            syntaxTree = parser.parse();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            return;
+        }
     }
 }
