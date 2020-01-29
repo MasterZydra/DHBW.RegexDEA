2895de21161d3186f1809a5bf44163bcd9552120
FHieser <felix_hieser@web.de>
2020-01-29 14:47:18 +0100

    Implementing FirstVisitor.java and adding and implementing FirstVisitorTest

--- src/test/Comparer.java
+++ src/test/Comparer.java
@@ -35,4 +35,43 @@ public final class Comparer {
         }
         throw new IllegalStateException("Invalid node type");
     }
+
+    public static boolean firstVisitorEquals(Visitable expected, Visitable visited)
+    {
+        if (expected == null && visited == null) return true;
+        if (expected == null || visited == null) return false;
+        if (expected.getClass() != visited.getClass()) return false;
+        if (expected.getClass() == BinOpNode.class)
+        {
+            BinOpNode op1 = (BinOpNode) expected;
+            BinOpNode op2 = (BinOpNode) visited;
+            return op1.nullable.equals(op2.nullable) &&
+                    op1.firstpos.equals(op2.firstpos) &&
+                    op1.lastpos.equals(op2.lastpos) &&
+                    firstVisitorEquals(op1.left, op2.left) &&
+                    firstVisitorEquals(op1.right, op2.right);
+        }
+        if (expected.getClass() == UnaryOpNode.class)
+        {
+            UnaryOpNode op1 = (UnaryOpNode) expected;
+            UnaryOpNode op2 = (UnaryOpNode) visited;
+            return op1.nullable.equals(op2.nullable) &&
+                    op1.firstpos.equals(op2.firstpos) &&
+                    op1.lastpos.equals(op2.lastpos) &&
+                    firstVisitorEquals(op1.subNode, op2.subNode);
+        }
+        if (expected.getClass() == OperandNode.class)
+        {
+            OperandNode op1 = (OperandNode) expected;
+            OperandNode op2 = (OperandNode) visited;
+            return op1.nullable.equals(op2.nullable) &&
+                    op1.firstpos.equals(op2.firstpos) &&
+                    op1.lastpos.equals(op2.lastpos);
+        }
+        throw new IllegalStateException(
+                String.format( "Beide Wurzelknoten sind Instanzen der Klasse %1$s !"
+                                + " Dies ist nicht erlaubt!",
+                        expected.getClass().getSimpleName())
+        );
+    }
 }
