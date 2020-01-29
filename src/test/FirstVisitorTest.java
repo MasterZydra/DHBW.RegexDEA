957b714c5db02805f406b1c433b8e5c2b2020a87
FHieser <felix_hieser@web.de>
2020-01-29 15:14:33 +0100

    Fixing missing Position in Tree

--- src/test/FirstVisitorTest.java
+++ src/test/FirstVisitorTest.java
@@ -17,11 +17,13 @@ public class FirstVisitorTest {
         ((SyntaxNode)right).nullable=false;
         ((SyntaxNode)right).firstpos.add(2);
         ((SyntaxNode)right).lastpos.add(2);
+        ((OperandNode)right).position=2;
 
         Visitable left = new OperandNode("a");
         ((SyntaxNode)left).nullable=false;
         ((SyntaxNode)left).firstpos.add(1);
         ((SyntaxNode)left).lastpos.add(1);
+        ((OperandNode)left).position=1;
 
         left = new BinOpNode("|", left, right);
         ((SyntaxNode)left).nullable=false;
@@ -41,6 +43,7 @@ public class FirstVisitorTest {
         ((SyntaxNode)right).nullable=false;
         ((SyntaxNode)right).firstpos.add(3);
         ((SyntaxNode)right).lastpos.add(3);
+        ((OperandNode)right).position=3;
 
         left = new BinOpNode("°", left, right);
         ((SyntaxNode)left).nullable=false;
@@ -53,7 +56,7 @@ public class FirstVisitorTest {
         ((SyntaxNode)right).nullable=false;
         ((SyntaxNode)right).firstpos.add(4);
         ((SyntaxNode)right).lastpos.add(4);
-
+        ((OperandNode)right).position=4;
 
         left = new BinOpNode("°", left, right);
         ((SyntaxNode)left).nullable=false;
@@ -67,7 +70,7 @@ public class FirstVisitorTest {
         ((SyntaxNode)right).nullable=false;
         ((SyntaxNode)right).firstpos.add(5);
         ((SyntaxNode)right).lastpos.add(5);
-
+        ((OperandNode)right).position=5;
 
         left = new BinOpNode("°", left, right);
         ((SyntaxNode)left).nullable=false;
@@ -81,7 +84,7 @@ public class FirstVisitorTest {
         ((SyntaxNode)right).nullable=false;
         ((SyntaxNode)right).firstpos.add(6);
         ((SyntaxNode)right).lastpos.add(6);
-
+        ((OperandNode)right).position=6;
 
         Visitable refTree = new BinOpNode("°", left, right);
         ((SyntaxNode)refTree).nullable=false;
@@ -92,16 +95,33 @@ public class FirstVisitorTest {
 
 
         right = new OperandNode("b");
+        ((OperandNode)right).position=2;
+
         left = new OperandNode("a");
+        ((OperandNode)left).position=1;
+
         left = new BinOpNode("|", left, right);
+
         left = new UnaryOpNode("*", left);
+
         right = new OperandNode("a");
+        ((OperandNode)right).position=3;
+
         left = new BinOpNode("°", left, right);
+
         right = new OperandNode("b");
+        ((OperandNode)right).position=4;
+
         left = new BinOpNode("°", left, right);
+
         right = new OperandNode("b");
+        ((OperandNode)right).position=5;
+
         left = new BinOpNode("°", left, right);
+
         right = new OperandNode("#");
+        ((OperandNode)right).position=6;
+        
         Visitable changeableTree = new BinOpNode("°", left, right);
 
         DepthFirstIterator.traverse(changeableTree,firstVisitor);
